7be4b158a97745a073a910379470d61443e1956d
SDR <50109097+dmytrosenyk@users.noreply.github.com>
2023-07-29 19:34:41 +0300

    change img path

--- src/main/java/com/puzzle/services/DefaultSettings.java
+++ src/main/java/com/puzzle/services/DefaultSettings.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class DefaultSettings {
-  public static String DEFAULT_IMG= "/Users/dmytrosenyk/Desktop/roadToJunior/testTaskPuzzle/src/main/resources/f1.jpg";
+  public static String DEFAULT_IMG= "src/main/resources/f1.jpg";
 
   public static int getHeight(BufferedImage img, int newWidth) {
 
