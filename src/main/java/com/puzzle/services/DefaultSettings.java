24f46750e77c042e3fd04232a7317c7b3f78c737
SDR <50109097+dmytrosenyk@users.noreply.github.com>
2023-07-29 19:46:00 +0300

    add img with 70 percent solving accuracy

--- src/main/java/com/puzzle/services/DefaultSettings.java
+++ src/main/java/com/puzzle/services/DefaultSettings.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class DefaultSettings {
-  public static String DEFAULT_IMG= "src/main/resources/f1.jpg";
+  public static String DEFAULT_IMG= "src/main/resources/100_percent_accuracy.jpg";
 
   public static int getHeight(BufferedImage img, int newWidth) {
 
